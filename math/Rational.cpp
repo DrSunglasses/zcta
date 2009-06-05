d6282514fd73e163ef0997f344fc4a8038c6d531
Jeffrey Bosboom <jbosboom@uci.edu>
2009-06-05 15:12:36 -0700

    Removed Rational cruft now that we're using mpq_class directly Moved hash_value and rational_cast to .cpp to reduce compile times

---
+++ math/Rational.cpp
@@ -0,0 +1,13 @@
+#include "Rational.hpp"
+#include <boost/functional/hash_fwd.hpp>
+
+std::size_t hash_value(const Rational& r) {
+	std::size_t seed = 0;
+	boost::hash_combine(seed, r.get_num());
+	boost::hash_combine(seed, r.get_den());
+	return seed;
+}
+
+double rational_cast(const Rational& r) {
+	return r.get_d();
+}
