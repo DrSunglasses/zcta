654f45ec9a7ba13b7ad8c4ae12f96fc44d468b63
Jeffrey Bosboom <jbosboom@uci.edu>
2009-06-17 12:53:06 -0700

    Print numerator of y coordinate instead of denominator in Printer.cpp

--- io/Printer.cpp
+++ io/Printer.cpp
@@ -6,7 +6,7 @@ void print(const std::string& outputFile, const boost::unordered_set<ZCTA>& zcta
 	out << zctas.size() << std::endl;
 	for (boost::unordered_set<ZCTA>::const_iterator i = zctas.cbegin(); i != zctas.cend(); ++i)
 		out << i->id() << " " << i->interiorPoint().x().get_num() << " "
-			<< i->interiorPoint().y().get_den() << std::endl;
+			<< i->interiorPoint().y().get_num() << std::endl;
 	out << adjacencies.size() << std::endl;
 	for (AdjacencySet::iterator i = adjacencies.begin(), end = adjacencies.end(); i != end; ++i)
 		out << i->first.id() << " " << i->second.id() << std::endl;
