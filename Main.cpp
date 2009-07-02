40bd4aa673906cbbbb64d02565eccf202075a058
Jeffrey Bosboom <jbosboom@uci.edu>
2009-07-02 14:12:36 -0700

    ADJACENCY_TOLERANCE macro, REMOVE_HH_ZCTAS and impl.

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <fstream>
 #include <streambuf>
-#include <set>
+#include <vector>
 #include <boost/lexical_cast.hpp>
 #include <boost/timer.hpp>
 #include <boost/unordered_set.hpp>
@@ -18,6 +18,9 @@
 #include "math/Rational.hpp"
 #include "io/Parser.hpp"
 #include "io/Printer.hpp"
+
+#define ADJACENCY_TOLERANCE 0.0001
+#define REMOVE_HH_ZCTAS 1
 
 int main(int argc, char** argv) {
 	std::cout << "Setting up custom memory allocation... " << std::endl;
@@ -36,14 +39,36 @@ int main(int argc, char** argv) {
 		std::cout << "Parsed in " << t1.elapsed() << std::endl;
 		std::cout << zctas.size() << " ZCTAs with " << segments.size() << " segments" << std::endl;
 
+#ifdef REMOVE_HH_ZCTAS
+		t1.restart();
+		{
+			std::vector<boost::unordered_set<ZCTA>::iterator> iters;
+			for (boost::unordered_set<ZCTA>::iterator i = zctas.begin(); i != zctas.end(); i++)
+				if (i->id().find("HH") != std::string::npos)
+					iters.push_back(i);
+			for (int i = 0; i < iters.size(); i++)
+				zctas.erase(iters[i]);
+		}
+		{
+			std::vector<boost::unordered_set<ZCTASegment>::iterator> iters;
+			for (boost::unordered_set<ZCTASegment>::iterator i = segments.begin(); i != segments.end(); i++)
+				if (i->zcta().id().find("HH") != std::string::npos)
+					iters.push_back(i);
+			for (int i = 0; i < iters.size(); i++)
+				segments.erase(iters[i]);
+		}
+		std::cout << "Removed HH ZCTAs in " << t1.elapsed() << std::endl;
+		std::cout << zctas.size() << " ZCTAs with " << segments.size() << " segments" << std::endl;
+#endif
+
 		AdjacencySet hadj;
-		HorizontalAlgorithm ha(segments, 0.001, &hadj);
+		HorizontalAlgorithm ha(segments, ADJACENCY_TOLERANCE, &hadj);
 		t1.restart();
 		ha();
 		std::cout << "Computed with horizontal sweep line in " << t1.elapsed() << ": " << hadj.size() << " adjacencies" << std::endl;
 		
 		AdjacencySet vadj;
-		VerticalAlgorithm va(segments, 0.001, &vadj);
+		VerticalAlgorithm va(segments, ADJACENCY_TOLERANCE, &vadj);
 		t1.restart();
 		va();
 		std::cout << "Computed with vertical sweep line in " << t1.elapsed() << ": " << vadj.size() << " adjacencies" << std::endl;
