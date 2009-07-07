591502b42bd6385ef06a00516438e06894b33ec3
Jeffrey Bosboom <jbosboom@uci.edu>
2009-07-06 19:08:16 -0700

    Removed some extra prints from TestVerticalComparators.cpp

--- algorithm/TestVerticalComparators.cpp
+++ algorithm/TestVerticalComparators.cpp
@@ -9,19 +9,19 @@ void testVerticalPointComparator() {
 	//c < a < d < b
 	assert(v(c, a) < 0);
 	assert(v(a, c) > 0);
-	
+
 	assert(v(a, d) < 0);
 	assert(v(d, a) > 0);
-	
+
 	assert(v(d, b) < 0);
 	assert(v(b, d) > 0);
-	
+
 	assert(v(c, d) < 0);
 	assert(v(d, c) > 0);
-	
+
 	assert(v(c, b) < 0);
 	assert(v(b, c) > 0);
-	
+
 	assert(v(a, b) < 0);
 	assert(v(b, a) > 0);
 }
@@ -31,9 +31,7 @@ void testVerticalLineComparator_thetaAdjustment() {
 	c.update(Point(1, 0));
 	Line l(Point(0, -1), Point(2, 1));
 	Line g(Point(0, 1), Point(2, -1));
-	
-	cout << l.theta() << endl << g.theta();
-	
+
 	assert(c(l, g) < 0);
 	assert(c(g, l) > 0);
 }
