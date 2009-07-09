0c8a4b3ca7c0d3a8e84c3ec3158a3216226736b6
Jeffrey Bosboom <jbosboom@uci.edu>
2009-07-09 14:34:59 -0700

    Add support for streaming boost::unordered_set in UnorderedStreaming

---
+++ io/UnorderedStreaming.hpp
@@ -0,0 +1,29 @@
+#ifndef UNORDEREDSTREAMING_HPP
+#define UNORDEREDSTREAMING_HPP
+
+//implement stream operators for boost::unordered_set<T>
+#include <boost/unordered_set.hpp>
+#include <iosfwd>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const boost::unordered_set<T>& set) {
+	out << set.size();
+	for (boost::unordered_set<T>::const_iterator i = set.cbegin(); i != set.cend(); ++i)
+		out << " " << *i;
+	return out;
+}
+
+template<typename T>
+std::istream& operator>>(std::istream& in, boost::unordered_set<T>& set) {
+	set.clear();
+	int elements = -1;
+	in >> elements;
+	for (int i = 0; i < elements; i++) {
+		T t;
+		in >> t;
+		set.insert(t);
+	}
+	return in;
+}
+
+#endif //UNORDEREDSTREAMING_HPP
