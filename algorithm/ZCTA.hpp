f9f13fe153dde2ca959e29a5b1db6eb38d930ce0
Jeffrey Bosboom <jbosboom@uci.edu>
2009-07-09 14:51:01 -0700

    Make ZCTA InputStreamable

--- algorithm/ZCTA.hpp
+++ algorithm/ZCTA.hpp
@@ -10,6 +10,7 @@
 
 #include <string>
 #include <utility>
+#include <iosfwd>
 #include "../math/Point.hpp"
 #include <boost/functional/hash.hpp>
 
@@ -18,6 +19,9 @@ private:
 	std::string id_;
 	Point interiorPoint_;
 public:
+	ZCTA() : id_("-BUG-"), interiorPoint_() {
+		//only provided to be InputStreamable, not for general use
+	}
 	ZCTA(const std::string& id, const Point& interiorPoint) : id_(id), interiorPoint_(interiorPoint) {
 		if (id.length() != 5)
 			throw std::logic_error("Bad ZCTA id: "+id);
@@ -40,10 +44,18 @@ public:
 	bool operator<(const ZCTA& z) const {
 		return id() < z.id();
 	}
+
+	friend std::istream& operator>>(std::istream& in, ZCTA& z) {
+		return in >> z.id_ >> z.interiorPoint_;
+	}
 };
 
 inline std::size_t hash_value(const ZCTA& z) {
 	return boost::hash<std::string>()(z.id());
+}
+
+inline std::ostream& operator<<(std::ostream& out, const ZCTA& z) {
+	return out << z.id() << " " << z.interiorPoint();
 }
 
 #endif	/* _ZCTA_HPP */
