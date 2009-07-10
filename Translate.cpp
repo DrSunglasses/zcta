1e621324f7484c0eea30abcd3708afeb58b27cff
Jeffrey Bosboom <jbosboom@uci.edu>
2009-07-09 19:59:33 -0700

    Translate.cpp now saves an unordered_map<ZCTA, vector<LineSegment> >
    This saves space by not repeatedly duplicating the ZCTA string.

--- Translate.cpp
+++ Translate.cpp
@@ -1,11 +1,13 @@
 //Translate.cpp: process name and data files
 
 #include "io/Parser.hpp"
-#include "io/UnorderedStreaming.hpp"
+#include "io/UnorderedSetStreaming.hpp"
+#include "io/UnorderedMapStreaming.hpp"
+#include "io/VectorStreaming.hpp"
 #include "algorithm/ZCTASegment.hpp"
-#include <boost/unordered_set.hpp>
 #include <iostream>
 #include <fstream>
+#include <utility>
 
 #define EXPECTED_ARGC 4
 #define NAME_FILE_INDEX 1
@@ -21,12 +23,21 @@ int main(int argc, char* argv[]) {
 
 	boost::unordered_set<ZCTA> zctas;
 	boost::unordered_set<ZCTASegment> segments;
-	std::cout << "Parsing from name file " << argv[NAME_FILE_INDEX] << " and data file " << argv[DATA_FILE_INDEX] << "... ";
+	std::cout << "Parsing from name file " << argv[NAME_FILE_INDEX] << " and data file " << argv[DATA_FILE_INDEX] << "... " << std::flush;
 	assembleZCTAs(argv[NAME_FILE_INDEX], argv[DATA_FILE_INDEX], &zctas, &segments);
 	std::cout << "done." << std::endl;
 
-	std::cout << "Writing output to " << argv[OUTPUT_FILE_INDEX] << "... ";
+	std::cout << "Sorting by ZCTA for serialization... " << std::flush;
+	boost::unordered_map<ZCTA, std::vector<LineSegment> > blob;
+	for (boost::unordered_set<ZCTA>::const_iterator i = zctas.cbegin(); i != zctas.cend(); ++i)
+		blob.insert(std::make_pair(*i, std::vector<LineSegment>()));
+	for (boost::unordered_set<ZCTASegment>::const_iterator i = segments.cbegin(); i != segments.cend(); ++i) {
+		blob[i->zcta()].push_back(*i); //this deliberately slices the ZCTASegment into a LineSegment
+	}
+	std::cout << "done." << std::endl;
+
+	std::cout << "Writing output to " << argv[OUTPUT_FILE_INDEX] << "... " << std::flush;
 	std::ofstream output(argv[OUTPUT_FILE_INDEX]);
-	output << zctas << " " << segments;
+	output << blob;
 	std::cout << "done." << std::endl;
 }
